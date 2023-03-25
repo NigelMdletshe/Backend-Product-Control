dc1071f8eea3c708fe3cca69b00bfadd28c9ad5a
Nigel Mdletshe <nigelmdletshe@gmail.com>
2023-03-25 20:47:03 +0200

    created django filter class

--- crm/accounts/views.py
+++ crm/accounts/views.py
@@ -29,7 +29,9 @@ def customer(request,pk_test):
     customer= Customer.objects.get(id=pk_test)
     orders = customer.order_set.all()
     order_count = orders.count()
+
     context = {'customer':customer,'orders':orders,'order_count':order_count}
+    
     return render(request,'accounts/customer.html',context)
 
 def createOrder(request, pk):
