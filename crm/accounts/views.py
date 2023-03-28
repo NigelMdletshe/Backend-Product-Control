74c7600abc9fb6e41dae0c41305b603a7281843a
Nigel Mdletshe <nigelmdletshe@gmail.com>
2023-03-28 17:15:06 +0200

    added login and registe html

--- crm/accounts/views.py
+++ crm/accounts/views.py
@@ -1,13 +1,20 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.forms import inlineformset_factory
+from django.contrib.auth.forms import UserCreationForm
 from .models import *
-from .forms import OrderForm
+from .forms import OrderForm, CreateUserForm
 from .filters import OrderFilter
 
 # Create your views here.
 def registerPage(request):
-    context = {}
+    form = CreateUserForm()
+    if request.method == 'POST':
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    context = {'form':form,'title':'Register'}
     return render(request,'accounts/register.html',context)
 
 def loginPage(request):
