43fb2312c8fbdcefd99e1e1b961043ad0b9ae96e
Nigel Mdletshe <nigelmdletshe@gmail.com>
2023-03-28 14:27:04 +0200

    implemented search filter by keywords

--- crm/crm/settings.py
+++ crm/crm/settings.py
@@ -119,7 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-MEDDIA_URL = 'images/'
+MEDIA_URL = 'images/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
