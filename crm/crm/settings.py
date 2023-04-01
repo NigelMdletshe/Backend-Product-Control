cd1d43364bf6bc3c3609f6ddce16bf5ed68b5c83
Nigel Mdletshe <nigelmdletshe@gmail.com>
2023-04-01 17:39:49 +0200

    reset password by email

--- crm/crm/settings.py
+++ crm/crm/settings.py
@@ -124,3 +124,11 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#SMTP Configuration
+EMAIL_BACKED = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_PORT = 587
+EMAIL_USE_TLS = "True"
+EMAIL_HOST_USER = "nigelmdletshe@gmail.com"
+EMAIL_HOST_PASSWORD = " your email password" # do not post your password on github 
